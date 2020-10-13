ae5b8afaf5e8ae0e2e132c9a21f80bcc293fdcee
Sarthak Thakur <50295928+SarthakThakur0097@users.noreply.github.com>
2020-10-13 11:03:23 -0400

    Added and Implemented Logging in and Registeration ActionMethod

---
+++ TheProgrammingInn/TheProgrammingInn.Com/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace TheProgrammingInn.Com.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Remote(action: "IsEmailInUse", controller: "Account")]
+        //Attribute used for custom domain validation
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password",
+            ErrorMessage = "Password and confirmation password do not match")]
+        public string ConfirmPassword { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
